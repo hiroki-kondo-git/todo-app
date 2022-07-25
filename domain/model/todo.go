13acdd260254455e8853c6b24db6e45c321b1cbf
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-07-25 20:51:56 +0900

    domain

--- domain/model/todo.go
+++ domain/model/todo.go
@@ -2,15 +2,16 @@ package model
 
 import "time"
 
+// status 1=todo, 2=done
 type Todo struct {
 	Id        int    `json:"id"`
 	Text      string `json:"text`
-	Status    string `json:"status"`
+	Status    int    `json:"status"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
-func NewTodo(text string, status string) *Todo {
+func NewTodo(text string, status int) *Todo {
 	todo := new(Todo)
 	todo.Text = text
 	todo.Status = status

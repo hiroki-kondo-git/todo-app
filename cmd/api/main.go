26ee40a50da07825035dfc6a820e2f361d0f7bab
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-07-25 20:51:39 +0900

    main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,14 +17,18 @@ func main() {
 	}
 	defer db.Close()
 
+	// DI
 	todoPersistence := persistence.NewTodoPersistence(db)
 	todoUseCase := usecase.NewTodoUseCase(todoPersistence)
 	todoHandler := todo_handler.NewTodoHandler(todoUseCase)
+	templateHandler := template_handler.NewTemplateHandler(todoHandler)
 
 	e := echo.New()
 	e.Renderer = template_handler.NewTemplate("templates/index.html")
 
-	e.GET("/", template_handler.Index)
+	e.GET("/", templateHandler.Index)
 	e.POST("/new", todoHandler.CreateTodo)
+	e.PUT("/update/:id", todoHandler.UpdateTodo)
+	e.DELETE("/delete/:id", todoHandler.DeleteTodo)
 	e.Logger.Fatal(e.Start(":8080"))
 }

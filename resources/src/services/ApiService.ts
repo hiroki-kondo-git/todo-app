a8fa3b40348dddb5bec9b1e4be014aacb98e77c0
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2022-08-13 19:08:40 +0900

    fix indent

--- resources/src/services/ApiService.ts
+++ resources/src/services/ApiService.ts
@@ -1,21 +1,21 @@
 import http from "@/http-common"
 
 class APiService {
-    getAll(): Promise<any> {
-        return http.get("/api/");
-    }
+	getAll(): Promise<any> {
+		return http.get("/api/");
+	}
 
-    create(data: any): Promise<any> {
-        return http.post("/api/new", data);
-    }
+	create(data: any): Promise<any> {
+		return http.post("/api/new", data);
+	}
 
-    update(id: any, data: any): Promise<any> {
-        return http.put(`/api/update/${id}`);
-    }
+	update(id: any, data: any): Promise<any> {
+		return http.put(`/api/update/${id}`);
+	}
 
-    delete(id: any): Promise<any> {
-        return http.delete(`/api/delete/${id}`);
-    }
+	delete(id: any): Promise<any> {
+		return http.delete(`/api/delete/${id}`);
+	}
 }
 
 export default new APiService();
--- resources/vue.config.js
+++ resources/vue.config.js
@@ -1,13 +1,13 @@
 // CORS対策
 module.exports = {
-    devServer: {
-        proxy: {
-            "^/api*": {
-                target: "http://localhost:8082",
-                pathRewrite: { "^/api": "" },
-                changeOrigin: true,
-                logLevel: "debug",
-            }
-        }
-    }
+	devServer: {
+		proxy: {
+			"^/api*": {
+				target: "http://localhost:8082",
+				pathRewrite: { "^/api": "" },
+				changeOrigin: true,
+				logLevel: "debug",
+			}
+		}
+	}
 };
